15b431a3f34b5b6fef04358e1a75950e5f2c2a1d
guslizikri <zikrizg7@gmail.com>
2024-03-24 12:08:07 +0700

    add feature add and delete phone_number

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,7 +3,11 @@ const routers = express.Router();
 const authRoutes = require("./auth");
 const userRoutes = require("./user");
 
+const phoneRoutes = require("./phone");
+
 routers.use("/user", userRoutes);
 routers.use("/auth", authRoutes);
+
+routers.use("/phone", phoneRoutes);
 
 module.exports = routers;
