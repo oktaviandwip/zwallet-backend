30368e3872ea745caf99fcc36289801c1aef79f8
saudahhasibuan <hrptaufik42@gmail.com>
2024-03-29 00:01:47 +0700

    melengkapai api user

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3001;
-const routers = require("./src/routers/index");
+const routers = require("./src/routers");
 const cors = require("cors");
 
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/", routers);
+app.use(routers);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
---
+++ src/middleware/auth.js
@@ -0,0 +1,22 @@
+const response = require("../utils/response");
+const jwt = require("jsonwebtoken");
+
+const middleware = {
+  authentication: (req, res, next) => {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      return response(res, 401, "Access denied. Log in to continue");
+    }
+    const token = authorization.replace("Bearer ", "");
+    jwt.verify(token, process.env.JWT_KEY, (error, decode) => {
+      if (error) {
+        return response(res, 401, error);
+      }
+      req.decodeToken = decode;
+      next();
+    });
+  },
+};
+
+module.exports = middleware;
