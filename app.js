17f5cead5835f8ce69ab7306c5632f3601ccda2d
Oktavian Dwi Putra <oktavian.dwiputra@gmail.com>
2024-06-27 09:50:52 +0700

    Router revision

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/image", express.static("./public/upload"));
 app.use(routers);
+app.use("/image", express.static("./public/upload"));
 
 db.connect()
   .then(() => {
