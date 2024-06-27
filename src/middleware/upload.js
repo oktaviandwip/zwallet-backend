65c7b38eeaddddfb4a2e01c2ec64cec5480adb14
Oktavian Dwi Putra <oktavian.dwiputra@gmail.com>
2024-06-28 05:20:29 +0700

    Multer revision

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -6,7 +6,7 @@ const middleware = {
   uploadUser: (req, res, next) => {
     const storage = multer.diskStorage({
       destination: function (req, file, cb) {
-        cb(null, "../../public/upload");
+        cb(null, "./public/upload");
       },
       filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
