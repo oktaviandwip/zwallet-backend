5a1af35d175e64491d9afe746ecde759c2501c5a
Oktavian Dwi Putra <oktavian.dwiputra@gmail.com>
2024-06-27 08:27:53 +0700

    Add CORS

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ const db = require("./src/config/db");
 const routers = require("./src/routers/index");
 const cors = require("cors");
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/image", express.static("./public/upload"));
 app.use(routers);
+const corsOptions = {
+  origin: "*", // Allow all origins
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow specific methods if needed
+  allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers if needed
+};
+app.use(cors(corsOptions));
 
 db.connect()
   .then(() => {
