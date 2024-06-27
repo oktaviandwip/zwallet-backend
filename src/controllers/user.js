9d6dac7bba0d6457b1e95c2f6fcd10aff2a6cad7
Oktavian Dwi Putra <oktavian.dwiputra@gmail.com>
2024-06-27 14:18:50 +0700

    Update photo profile revision

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -111,7 +111,7 @@ controllers.updatePin = async (req, res) => {
 // Update Photo Profile
 controllers.updatePhoto = async (req, res) => {
   try {
-    const image = `https://zwallet-backend-production.up.railway.app/image/${req.file.filename}`;
+    // const image = `https://zwallet-backend-production.up.railway.app/image/${req.file.filename}`;
     const { rows } = await models.getPassByEmail(req.body.email);
     if (rows.length === 0) {
       return response(res, 404, "Data not found");
